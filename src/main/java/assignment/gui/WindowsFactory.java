727d83b2091b782e86b15f092cfdbb05f88ba56b
Ataly001 <ataly9808@gmail.com>
2026-09-19 23:27:52 +0500

    adding WindowsFactory

---
+++ src/main/java/assignment/gui/WindowsFactory.java
@@ -0,0 +1,12 @@
+package assignment.gui;
+
+public class WindowsFactory implements GUIFactory {
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+    @Override
+    public Checkbox createCheckbox() {
+        return new WindowsCheckbox();
+    }
+}
